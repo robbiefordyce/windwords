e551a3179aa59d962ee452999948933ded050b93
Robbie Fordyce <fordycerobbie@gmail.com>
2022-01-04 11:58:31 +1300

    Added CRUD methods to mongo utilities module

--- src/python/windwords/constants.py
+++ src/python/windwords/constants.py
@@ -2,4 +2,15 @@
     A collection of constants pertaining to the windwords project.
 """
 
-DEFAULT_DATABASE_NAME = "windwords_db"
+
+class Database:
+    DEFAULT_NAME = "windwords_db"
+
+
+class Collection:
+    BOOKS = "books"
+    CHANNELS = "channels"
+    CHURCHES = "churches"
+    DENOMINATIONS = "denominations"
+    RESOURCES = "resources"
+    SERMONS = "sermons"
--- src/python/windwords/mongo.py
+++ src/python/windwords/mongo.py
@@ -47,19 +47,112 @@ def connect_to_mongo_client(username=None, password=None, cluster=None):
     )
 
 
-def insert_documents(collection, documents, database=None):
+def get_collection(name):
+    """ Returns the MongoDB collection by name. 
+
+    Args:
+        name (str): The collection name.
+    Returns:
+        pymongo.Collection: The MongoDB collection instance.
+    """
+    client = get_client()
+    database = client.get_database(constants.Database.DEFAULT_NAME)
+    return database.get_collection(name)
+
+
+def find_documents(collection, query, limit=None, sort=None):
+    """ Returns the documents that match the query in the specified collection.
+
+    Args:
+        collection (str): The collection name to query.
+        query (dict): The document criteria to match.
+        limit (int, optional): If specified, limits the results to this number.
+        sort (list(tuple(str, int)), optional): If specified, sorts the results
+            by mutliple fields. Each field is specified by pairs of the field
+            name (str) and the direction (ASCENDING=1, DESCENDING=-1).
+    Yields:
+        Dict: The matched document, or None.
+    """
+    cursor = get_collection(collection).find(query)
+    if limit:
+        cursor.limit(limit)
+    if sort:
+        cursor.sort(sort)
+    for document in cursor:
+        yield document
+
+
+def find_document(collection, query):
+    """ Returns the first document that matches the query in the specified
+        collection.
+
+    Args:
+        collection (str): The collection name to query.
+        query (dict): The document criteria to match.
+    Returns:
+        Dict: The matched document, or None.
+    """
+    return get_collection(collection).find_one(query)
+
+
+def find_document_by_id(collection, objectID):
+    """ Returns the document that matches the specified objectID.
+
+    Args:
+        collection (str): The collection name to insert into.
+        objectID (bson.ObjectId): The database object identifier.
+    Returns:
+        Dict: The matched document, or None.
+    """
+    return find_one_document(collection, {"_id", objectID})
+
+
+def find_all(collection):
+    """ Returns all documents within the specified collection.
+
+    Args:
+        collection (str): The collection name to query.
+    Yields:
+        Dict: Documents within the collection.
+    """
+    return find_documents(collection, {})
+
+
+def count_documents(collection, query):
+    """ Returns the total number of documents, within the specified collection,
+        that match the provided criteria.
+
+    Args:
+        collection (str): The collection name.
+        query (dict): The document criteria to match.
+    Returns:
+        int: The number of documents that match the query criteria.
+    """
+    return get_collection(collection).count_documents(query)
+
+
+def count_collection(collection):
+    """ Returns the total number of documents within the specified collection.
+
+    Args:
+        collection (str): The collection name.
+    Returns:
+        int: The number of documents within the collection.
+    """
+    return count_documents(collection, {})
+    
+
+def insert_documents(collection, documents):
     """ Inserts the provided documents into the specified database collection.
 
     Args:
         collection (str): The collection name to insert into.
         documents (dict): The documents to insert.
-        database (str, optional): The database name.
-            Defaults to the `DEFAULT_DATABASE_NAME` constant.  
     Returns:
         List[str]: The inserted document object ids.
     """
-    client = get_client()
-    database = client.get_database(database or constants.DEFAULT_DATABASE_NAME)
-    collection = database.get_collection(collection)
+    if isinstance(documents, dict):
+        documents = [documents]
+    collection = get_collection(collection)
     result = collection.insert_many(documents)
     return result.inserted_ids
